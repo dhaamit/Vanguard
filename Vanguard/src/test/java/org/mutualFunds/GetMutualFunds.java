5dc10ac01420d8be946e21bb536c71fbea9f4aad
Amit Dharmale <amit.20july@gmail.comgit config --global user.email amit.20july@gmail.com>
2024-12-01 12:18:10 -0500

    first commit

---
+++ Vanguard/src/test/java/org/mutualFunds/GetMutualFunds.java
@@ -0,0 +1,12 @@
+//created by @dhaamit
+package org.mutualFunds;
+
+import org.testng.annotations.Test;
+
+public class GetMutualFunds {
+    @Test
+    public void getMutualFunds()
+    {
+        System.out.println("inside Get Mutual Funds");
+    }
+}
