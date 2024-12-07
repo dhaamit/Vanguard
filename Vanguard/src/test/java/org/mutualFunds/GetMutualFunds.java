e1cc0f8c78953cb931d26721a19a21f5614eac17
Amit Dharmale <amit.20july@gmail.comgit config --global user.email amit.20july@gmail.com>
2024-12-07 11:23:57 -0500

    added testSuite

--- Vanguard/src/test/java/org/mutualFunds/GetMutualFunds.java
+++ Vanguard/src/test/java/org/mutualFunds/GetMutualFunds.java
@@ -5,6 +5,7 @@ import org.base.BaseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.sql.Driver;
@@ -12,13 +13,23 @@ import java.sql.Driver;
 public class GetMutualFunds extends BaseTest {
     final String mutualFundUrl = "/investment-products/list/mutual-funds";
     WebDriver driver;
-    @Test
-    public void getMutualFunds()
-    {
+
+    @Test(groups = {"regression", "functional"})
+    public void getMutualFunds() {
         String title;
-    //        driver.get("https://investor.vanguard.com" + mutualFundUrl);
+        //        driver.get("https://investor.vanguard.com" + mutualFundUrl);
         driver = super.getDriver();
         title = driver.findElement(By.xpath("//div[@class='discover-title']")).getText();
         System.out.println("inside Get Mutual Funds : " + title);
+    }
+
+    @Test(groups = {"regression", "functional"})
+    public void secondFunction() {
+        System.out.println("second Test");
+    }
+
+    @AfterTest()
+    public void closeTest() {
+        driver.close();
     }
 }
