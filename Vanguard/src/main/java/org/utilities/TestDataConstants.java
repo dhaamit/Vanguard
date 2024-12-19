33c935cedff94b6be4d62efa70d158108c0cc438
Amit Dharmale <amit.20july@gmail.comgit config --global user.email amit.20july@gmail.com>
2024-12-18 19:55:04 -0500

    updated testDataConstants

--- Vanguard/src/main/java/org/utilities/TestDataConstants.java
+++ Vanguard/src/main/java/org/utilities/TestDataConstants.java
@@ -2,6 +2,8 @@
 package org.utilities;
 
 public class TestDataConstants {
+    /* Constant for Mutual Funds URL */
     public static final String MUTUALFUNDSURL = "/investment-products/list/mutual-funds";
+    /* Constant for Corporate Funds URL */
     public static final String CORPORATEFUNDSURL="/corporate-portal";
 }
